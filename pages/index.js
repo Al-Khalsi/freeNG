fe90c436ed0acfc528e0bed2eaa430bbfdaf710e
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-10-14 03:34:51 +0330

    folder structure

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,22 @@ import Link from 'next/link';
 
 function index() {
   return (
-    <div>
+    <div className="main">
+      <header className='header'>
+
+        <div className='bg-logo'>
+          <div className='logo'></div>
+        </div>
+
+        <div className='search-box'>
+          <input type="search" />
+        </div>
+
+        <div className='right-header'>
+          <button></button>
+          <Link href="/profile">profile</Link>
+        </div>
+      </header>
       <h1>home page</h1>
       <Link href="/product/1">Go to product page 1</Link>
       <br />
---
+++ pages/profile/[id].js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+
+const Profile = () => {
+    const router = useRouter();
+    const { id } = router.query;
+
+    return (
+        <div>
+            <h1>page product</h1>
+            <p>This page is about product number {id}.</p>
+        </div>
+    );
+};
+
+export default Profile;
