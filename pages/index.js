f15a9b3ac0d7e035c5741b498754b58ef7b794b1
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-10-15 13:30:43 +0330

    add particles package

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,38 @@
+
+import ParticlesComponent from '@/components/templates/background/particles';
 import Link from 'next/link';
+import { FaSun, FaMoon } from "react-icons/fa";
 
 function index() {
   return (
-    <div className="main">
-      <header className='header'>
+    <>
+      <ParticlesComponent id='particles' />
+      <div className="main">
+        <header className='header'>
 
-        <div className='bg-logo'>
-          <div className='logo'></div>
-        </div>
+          <div className='bg-logo'>
+            <div className='logo'>LOGO</div>
+          </div>
 
-        <div className='search-box'>
-          <input type="search" />
-        </div>
+          <div className='search-box'>
+            <input type="search" className='search' placeholder='search...' />
+          </div>
 
-        <div className='right-header'>
-          <button></button>
-          <Link href="/profile">profile</Link>
-        </div>
-      </header>
-      <h1>home page</h1>
-      <Link href="/product/1">Go to product page 1</Link>
-      <br />
-      <Link href="/product/2">Go to product page 2</Link>
-    </div>
+          <div className='right-header'>
+            <button className='change-background'>
+              <FaMoon />
+            </button>
+            <Link href="/profile">
+              profile
+            </Link>
+          </div>
+        </header>
+        <h1>home page</h1>
+        <Link href="/product/1">Go to product page 1</Link>
+        <br />
+        <Link href="/product/2">Go to product page 2</Link>
+      </div>
+    </>
   )
 }
 
