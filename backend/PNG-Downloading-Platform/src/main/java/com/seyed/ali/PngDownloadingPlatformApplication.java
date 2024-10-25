e494335a470d572020e41544caab2ad723ca14e0
SayedAli <sayedxali7@gmail.com>
2024-10-25 21:11:52 +0330

    (bk)initial commit

---
+++ backend/PNG-Downloading-Platform/src/main/java/com/seyed/ali/PngDownloadingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.seyed.ali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PngDownloadingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PngDownloadingPlatformApplication.class, args);
+	}
+
+}
