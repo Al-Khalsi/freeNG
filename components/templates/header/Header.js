a67cfc31e5961fbd3544096b4ecf2393b40486db
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-02 15:40:37 +0330

    logo ui

--- components/templates/header/Header.js
+++ components/templates/header/Header.js
@@ -6,8 +6,20 @@ function Header({ token, username, email, userId, handleLogout }) {
         <header className='header w-full h-24 overflow-hidden px-2 md:px-12 flex justify-between items-center text-white'>
 
             <div className='bg-logo'>
+                <div class="points_wrapper">
+                    <i class="point"></i>
+                    <i class="point"></i>
+                    <i class="point"></i>
+                    <i class="point"></i>
+                    <i class="point"></i>
+                    <i class="point"></i>
+                    <i class="point"></i>
+                    <i class="point"></i>
+                    <i class="point"></i>
+                    <i class="point"></i>
+                </div>
                 <div className='logo'>
-                    I<span>m</span>Alchem<span>y</span>
+                    I<span className='burnt'>m</span>Alchem<span>y</span>
                 </div>
             </div>
 
@@ -63,45 +75,43 @@ function Header({ token, username, email, userId, handleLogout }) {
 
             </div>
 
-            <div className='right-header flex py-1'>
-                <button className='sm:hidden flex justify-center items-center w-10 h-10 mr-4 rounded-full text-xl bg-black text-white'>
-                    <FaSearch />
-                </button>
-                {token ? ( // Check if token exists
-                    <div>
-                        <Link href={`/profile/${userId}?username=${encodeURIComponent(username.trim())}&email=${encodeURIComponent(email.trim())}`}>
-                            <span className='text-white'>{username}</span>
-                        </Link>
-                        <button onClick={handleLogout} className='ml-4 px-4 py-2 rounded-lg bg-red-500 text-white'>
-                            Logout
-                        </button>
-                    </div>
-                ) : (
-                    <button type="button" class="button rounded-xl">
-                        <span class="fold"></span>
-
-                        <div class="points_wrapper">
-                            <i class="point"></i>
-                            <i class="point"></i>
-                            <i class="point"></i>
-                            <i class="point"></i>
-                            <i class="point"></i>
-                            <i class="point"></i>
-                            <i class="point"></i>
-                            <i class="point"></i>
-                            <i class="point"></i>
-                            <i class="point"></i>
-                        </div>
-
-                        <span class="inner">
-                            <Link href="/validation" className=''>
-                                Login / Register
-                            </Link>
-                        </span>
+            <button className='sm:hidden flex justify-center items-center w-10 h-10 mr-4 rounded-full text-xl bg-black text-white'>
+                <FaSearch />
+            </button>
+            {token ? ( // Check if token exists
+                <div>
+                    <Link href={`/profile/${userId}?username=${encodeURIComponent(username.trim())}&email=${encodeURIComponent(email.trim())}`}>
+                        <span className='text-white'>{username}</span>
+                    </Link>
+                    <button onClick={handleLogout} className='ml-4 px-4 py-2 rounded-lg bg-red-500 text-white'>
+                        Logout
                     </button>
+                </div>
+            ) : (
+                <button type="button" class="button rounded-xl">
+                    <span class="fold"></span>
 
-                )}
-            </div>
+                    <div class="points_wrapper">
+                        <i class="point"></i>
+                        <i class="point"></i>
+                        <i class="point"></i>
+                        <i class="point"></i>
+                        <i class="point"></i>
+                        <i class="point"></i>
+                        <i class="point"></i>
+                        <i class="point"></i>
+                        <i class="point"></i>
+                        <i class="point"></i>
+                    </div>
+
+                    <span class="inner">
+                        <Link href="/validation" className=''>
+                            Login / Register
+                        </Link>
+                    </span>
+                </button>
+
+            )}
         </header>
     )
 }
