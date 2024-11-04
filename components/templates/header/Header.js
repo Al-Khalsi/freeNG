4a9c5ca90beba2cfec495e1f56d7fecf39901cdb
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-04 04:08:10 +0330

    css ui

--- components/templates/header/Header.js
+++ components/templates/header/Header.js
@@ -3,7 +3,7 @@ import { FaSearch, FaMicrophone } from 'react-icons/fa';
 
 function Header({ token, username, email, userId, handleLogout }) {
     return (
-        <header className='header w-full h-24 overflow-hidden px-2 md:px-12 flex justify-between items-center text-white'>
+        <header className='header w-full h-24 px-2 md:px-12 flex justify-between items-center text-white'>
 
             <div className='bg-logo'>
                 <div class="points_wrapper">
