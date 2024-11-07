bef41e7695152a70751eaf1768ef557c5d4117ef
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-07 11:31:46 +0330

    fix error ui in login and register

--- pages/Validation/index.js
+++ pages/Validation/index.js
@@ -102,9 +102,9 @@ function AuthForm() {
     };
 
     return (
-        <div className='Validation w-full h-full flex justify-center items-center min-h-screen bg-gradient-to-r from-bgPurple to-bgLightPurple'>
+        <div className='Validation w-full h-full flex justify-center items-center min-h-screen bg-bgDarkBlue'>
             <div className={`wrapper relative w-custom-212 h-custom-136 m-5 bg-bgDarkGray rounded-3xl shadow-xl overflow-hidden ${isActive ? 'active' : ''}`}>
-                <div className='login form-box absolute right-0 w-1/2 h-full flex items-center p-10 bg-bgDarkGray text-center z-10'>
+                <div className='login form-box absolute right-0 w-1/2 h-full flex items-center p-10 bg-gradient-to-t from-bgPurple to-bgLightPurple text-center z-10'>
                     <form onSubmit={handleLogin} className='w-full'>
                         <h2 className='text-4xl my-2.5'>Login</h2>
                         {error && <p className='text-red-500'>{error}</p>}
@@ -113,7 +113,7 @@ function AuthForm() {
                                 name="username"
                                 value={credentials.username}
                                 onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-inputwhite rounded-lg border-none outline-none text-base font-medium'
+                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-base font-medium'
                                 placeholder='Username' required />
                             <FaUser className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
                         </div>
@@ -122,22 +122,22 @@ function AuthForm() {
                                 name="password"
                                 value={credentials.password}
                                 onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-inputwhite rounded-lg border-none outline-none text-base font-medium placeholder-font-normal'
+                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-base font-medium placeholder-font-normal'
                                 placeholder='Password' required />
                             <FaLock className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
                         </div>
                         <div className='forgot-link -mt-3.5 mb-3.5'>
                             <p className='text-base no-underline'>Forgot password?</p>
                         </div>
-                        <button type='submit' className='btn w-full h-12 rounded-lg bg-darkBlue text-white shadow-lg border-none text-base font-semibold cursor-pointer'>Login</button>
+                        <button type='submit' className='btn w-full h-12 rounded-lg bg-bgDarkBlue text-white shadow-lg border-none text-base font-semibold cursor-pointer'>Login</button>
                         <p className='text-base my-4'>or</p>
                         <div className='social-icon'>
-                            <p className='w-full h-12 rounded-lg flex justify-center items-center bg-lightGray shadow-lg text-lg'><FcGoogle className='mr-2 text-2xl' />Login With Google</p>
+                            <p className='w-full h-12 rounded-lg flex justify-center items-center bg-bgWhite text-clBlack shadow-lg text-lg'><FcGoogle className='mr-2 text-2xl' />Login With Google</p>
                         </div>
                     </form>
                 </div>
 
-                <div className='register form-box absolute right-0 w-1/2 h-full flex items-center p-10 bg-white text-center z-10'>
+                <div className='register form-box absolute right-0 w-1/2 h-full flex items-center p-10 bg-gradient-to-t from-bgPurple to-bgLightPurple text-center z-10'>
                     <form onSubmit={handleRegistration} className='w-full'>
                         <h2 className='text-4xl my-2.5'>Register</h2>
                         {error && <p className='text-red-500'>{error}</p>}
@@ -146,7 +146,7 @@ function AuthForm() {
                                 name="username"
                                 value={credentials.username}
                                 onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-inputwhite rounded-lg border-none outline-none text-base font-medium'
+                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-base font-medium'
                                 placeholder='Username' required />
                             <FaUser className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
                         </div>
@@ -155,7 +155,7 @@ function AuthForm() {
                                 name="email"
                                 value={credentials.email}
                                 onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-inputwhite rounded-lg border-none outline-none text-base font-medium'
+                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-base font-medium'
                                 placeholder='Email' required />
                             <MdEmail className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
                         </div>
@@ -164,22 +164,22 @@ function AuthForm() {
                                 name="password"
                                 value={credentials.password}
                                 onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-inputwhite rounded-lg border-none outline-none text-base font-medium placeholder-font-normal'
+                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-base font-medium placeholder-font-normal'
                                 placeholder='Password' required />
                             <FaLock className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
                         </div>
                         <button type='submit'
-                            className='btn w-full h-12 rounded-lg bg-darkBlue text-white shadow-lg border-none text-base font-semibold cursor-pointer'>
+                            className='btn w-full h-12 rounded-lg bg-bgDarkBlue text-white shadow-lg border-none text-base font-semibold cursor-pointer'>
                             Register
                         </button>
                         <p className='text-base my-4'>or</p>
                         <div className='social-icon'>
-                            <p className='w-full h-12 rounded-lg flex justify-center items-center bg-lightGray shadow-lg text-lg'><FcGoogle className='mr-2 text-2xl' /> Register With Google</p>
+                            <p className='w-full h-12 rounded-lg flex justify-center items-center bg-bgWhite text-clBlack shadow-lg text-lg'><FcGoogle className='mr-2 text-2xl' /> Register With Google</p>
                         </div>
                     </form>
                 </div>
 
-                <div className="toggle-form absolute left-0 w-full h-full">
+                <div className="toggle-form absolute left-0 w-full h-full bg-gradient-to-t from-bgPurple to-bgLightPurple">
                     <div className='toggle-panel toggle-left absolute left-0 w-1/2 h-full flex flex-col justify-center items-center text-white z-20'>
                         <h4 className='text-3xl mb-2 hidden md:block'>Hello, Welcome!</h4>
                         <p className='mb-5 hidden md:block'>Don't have an account?</p>
