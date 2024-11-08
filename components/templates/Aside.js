b66c9b5914db903f4202c52375d30ca02fe50eb0
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-09 02:33:01 +0330

    install package axios api

--- components/templates/Aside.js
+++ components/templates/Aside.js
@@ -2,32 +2,9 @@ import SelectWithSearch from '../modules/SelectWithSearch';
 
 const Aside = ({ openSelect, handleSelectToggle, options }) => {
     return (
-        <aside className='filter-sidebar flex flex-col w-1/6 bg-bgDarkGray2 rounded-2xl mr-8'>
-            <div className='filtering'>
-                <SelectWithSearch
-                    options={options[0]}
-                    defaultText="Categories"
-                    isOpen={openSelect === 1}
-                    onToggle={() => handleSelectToggle(1)}
-                />
-                <SelectWithSearch
-                    options={options[1]}
-                    defaultText="Sub Categories"
-                    isOpen={openSelect === 2}
-                    onToggle={() => handleSelectToggle(2)}
-                />
-                <SelectWithSearch
-                    options={options[2]}
-                    defaultText="Type"
-                    isOpen={openSelect === 3}
-                    onToggle={() => handleSelectToggle(3)}
-                />
-                <SelectWithSearch
-                    options={options[3]}
-                    defaultText="Color"
-                    isOpen={openSelect === 4}
-                    onToggle={() => handleSelectToggle(4)}
-                />
+        <aside className='filter-sidebar flex flex-col w-1/6 mr-8'>
+            <div className='filtering bg-bgDarkGray2 rounded-xl'>
+               ssssssssssssss <br /> ssssssssssssssss <br /> 
             </div>
         </aside>
     );
