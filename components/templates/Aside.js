093094cc4b5c3d2d89d74d84642fa8a4d25b7345
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-09 23:29:44 +0330

    filter ui

--- components/templates/Aside.js
+++ components/templates/Aside.js
@@ -1,41 +1,80 @@
 import { BiSolidCategory } from "react-icons/bi";
-import { PiTreeViewFill, PiTreeStructureFill } from "react-icons/pi";
-import { IoIosArrowDown } from "react-icons/io";
+import { PiTreeViewFill } from "react-icons/pi";
+import { IoIosArrowDown, IoMdColorPalette } from "react-icons/io";
+import { FaShapes } from "react-icons/fa";
+
+
 
 const Aside = () => {
     return (
         <aside className='filter-sidebar flex flex-col w-1/6 mr-8'>
-            <div className='filtering bg-bgDarkGray2 rounded-xl'>
-               <ul>
-                <li>
-                    <button className="dropdown-btn flex items-center gap-4 rounded-lg p-3 no-underline">
-                        <BiSolidCategory />
-                        <span className="grow">Category</span>
-                        <IoIosArrowDown className="shrink-0"/>
-                    </button>
-                    <ul className="sub-menu">
-                        <li>test 1</li>
-                        <li>test 2</li>
-                        <li>test 3</li>
-                        <li>test 4</li>
-                        <li>test 5</li>
-                    </ul>
-                </li>
-                <li>
-                    <button className="dropdown-btn">
-                        <PiTreeViewFill />
-                        <span>Sub Category</span>
-                        <IoIosArrowDown />
-                    </button>
-                    <ul className="sub-menu">
-                        <li>test 1</li>
-                        <li>test 2</li>
-                        <li>test 3</li>
-                        <li>test 4</li>
-                        <li>test 5</li>
-                    </ul>
-                </li>
-               </ul>
+            <div className='filtering bg-bgDarkGray2 rounded-xl px-2 py-2'>
+                <ul>
+                    <li>
+                        <button className="dropdown-btn w-full flex justify-between items-center gap-1 rounded-lg p-3 no-underline">
+                            <div className="flex items-center gap-3">
+                                <BiSolidCategory />
+                                <span className="grow">Category</span>
+                            </div>
+                            <IoIosArrowDown className="shrink-0" />
+                        </button>
+                        <ul className="sub-menu hidden h-0">
+                            <li>test 1</li>
+                            <li>test 2</li>
+                            <li>test 3</li>
+                            <li>test 4</li>
+                            <li>test 5</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <button className="dropdown-btn w-full flex justify-between items-center gap-1 rounded-lg p-3 no-underline">
+                            <div className="flex items-center gap-3">
+                                <PiTreeViewFill />
+                                <span>Sub Category</span>
+                            </div>
+                            <IoIosArrowDown />
+                        </button>
+                        <ul className="sub-menu hidden h-0">
+                            <li>test 1</li>
+                            <li>test 2</li>
+                            <li>test 3</li>
+                            <li>test 4</li>
+                            <li>test 5</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <button className="dropdown-btn w-full flex justify-between items-center gap-1 rounded-lg p-3 no-underline">
+                            <div className="flex items-center gap-3">
+                                <FaShapes />
+                                <span>Style PNG</span>
+                            </div>
+                            <IoIosArrowDown />
+                        </button>
+                        <ul className="sub-menu hidden h-0">
+                            <li>test 1</li>
+                            <li>test 2</li>
+                            <li>test 3</li>
+                            <li>test 4</li>
+                            <li>test 5</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <button className="dropdown-btn w-full flex justify-between items-center gap-1 rounded-lg p-3 no-underline">
+                            <div className="flex items-center gap-3">
+                                <IoMdColorPalette className="text-lg" />
+                                <span>Color</span>
+                            </div>
+                            <IoIosArrowDown />
+                        </button>
+                        <ul className="sub-menu hidden h-0">
+                            <li>test 1</li>
+                            <li>test 2</li>
+                            <li>test 3</li>
+                            <li>test 4</li>
+                            <li>test 5</li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </aside>
     );
