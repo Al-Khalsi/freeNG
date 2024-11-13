d48268fb764f2e31d7eb69e5a4e35886947d6fef
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-14 00:09:18 +0330

    add component card

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@
 import Link from 'next/link';
 import { useAuth } from '../context/AuthContext';
 import { useEffect, useState } from "react";
-import { MdFullscreen } from "react-icons/md";
 import Images from "../data/db.json";
 import { useRouter } from 'next/router';
 import Header from '@/components/templates/Header';
@@ -10,6 +9,7 @@ import Aside from '@/components/templates/Aside';
 import { apiFetch } from '../utils/api'; // Import the apiFetch function
 import Footer from '@/components/templates/Footer';
 import MouseEffect from '@/components/modules/MouseEffect';
+import Card from '@/components/templates/Card';
 
 function Index() {
   const { token, username, email, clearToken, userId } = useAuth(); // Destructure token and username from Auth context
@@ -203,40 +203,7 @@ function Index() {
           />
           <section className='grid gap-8 w-5/6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
             {currentImages.map((image) => (
-              // <Link href={`/product/${image.id}
-              // ?title=${encodeURIComponent(image.Title)}
-              // &size=${encodeURIComponent(image.Size)}
-              // &dimensions=${encodeURIComponent(image.Dimensions)}
-              // &download=${encodeURIComponent(image.Download)}
-              // &src=${encodeURIComponent(image.Src)}`} key={image.id}>
-              <div className='card w-full h-80 rounded-lg overflow-hidden bg-bgDarkGray' key={image.id}>
-                <div className='inside-card h-full w-full p-3'>
-                  <div className='bg-img relative w-full h-2/3 flex justify-center items-center p-2 rounded-md'>
-                    <div className='absolute top-2 right-2 bg-darkBlue text-white p-1 rounded-md opacity-60'>
-                      <MdFullscreen className='text-xl' />
-                    </div>
-                    <img src={image.Src} alt={image.Title} className='w-full h-full object-continer' />
-                  </div>
-                  <div className='info-img w-full h-1/3 px-2 py-3'>
-                    <h3 className='block text-xl text-white text-ellipsis overflow-hidden whitespace-nowrap'>{image.Title}</h3>
-                    <div className='flex justify-between mt-3'>
-                      <div className='flex flex-col w-1/3 text-center pr-2 text-lightBlue'>
-                        <span className='block text-sm'>{image.Size}</span>
-                        <span className='text-xs'>Size</span>
-                      </div>
-                      <div className='flex flex-col w-1/3 text-center text-lightBlue border-x-2 border-lightGray'>
-                        <span className='block text-sm'>{image.Dimensions}</span>
-                        <span className='text-xs'>Dimensions</span>
-                      </div>
-                      <div className='flex flex-col w-1/3 text-center pl-2 text-lightBlue'>
-                        <span className='block text-sm'>{image.Download}</span>
-                        <span className='text-xs'>Download</span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              // </Link>
+               <Card key={image.id} image={image} />
             ))}
           </section>
         </main>
