3771ef1097a96607396e9b65d771244f1d045db8
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-16 18:47:29 +0330

    add card in rows xl:grid-cols-5

--- pages/index.js
+++ pages/index.js
@@ -206,7 +206,7 @@ function Index() {
                 handleSelectToggle={handleSelectToggle}
                 options={[options1, options2, options3, options4]} // Pass options as an array
             /> */}
-            <section className='grid gap-6 w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+            <section className='grid gap-6 w-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
               {currentImages.map((image) => (
                   <Card key={image.id} image={image} />
               ))}
