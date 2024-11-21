3228a85986495deeb31173b1f85df2316b566828
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-21 19:46:51 +0330

    ui link button download

--- components/templates/Card.js
+++ components/templates/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MdFullscreen } from "react-icons/md";
-import { FaCloudDownloadAlt, FaImage } from "react-icons/fa";
+import { FaImage } from "react-icons/fa";
 import { RxDimensions } from "react-icons/rx";
 import Link from 'next/link';
 
@@ -12,7 +12,7 @@ function Card({ image }) {
         <div className='card w-full rounded-lg overflow-hidden bg-bgDarkGray'>
             <div className='inside-card w-full px-3 pt-3'>
                 <div className='bg-img relative w-full h-52 flex justify-center items-center p-2 rounded-md'>
-                    <div className='absolute top-2 right-2 bg-darkBlue text-white p-1 rounded-md opacity-60'>
+                    <div className='absolute top-2 right-2  bg-darkBlue text-white p-1 rounded-md opacity-60'>
                         <MdFullscreen className='text-xl' />
                     </div>
                     <img src={`../../img/${image.path}`} alt={image.fileTitle} className='w-full h-full object-contain' />
@@ -27,10 +27,6 @@ function Card({ image }) {
                         <div className='flex justify-between items-center bg-gray-600 ml-2 px-2 py-1 rounded text-xs text-lightBlue'>
                             <RxDimensions />
                             <span className='block ml-1'>{image.dimensions}</span>
-                        </div>
-                        <div className='flex justify-between items-center bg-gray-600 ml-2 px-2 py-1 rounded text-xs text-lightBlue'>
-                            <FaCloudDownloadAlt />
-                            <span className='block ml-1'>{image.download}</span>
                         </div>
                     </div>
                     {/* <button type='button' className='w-full bg-bgDarkBlue rounded mt-3 py-2'>
