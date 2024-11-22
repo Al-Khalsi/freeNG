293b763df53462a858386c6aa458f802a3897d17
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-22 12:01:47 +0330

    add full screen image modal

--- components/templates/Card.js
+++ components/templates/Card.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import { MdFullscreen } from "react-icons/md";
 import { FaImage } from "react-icons/fa";
 import { RxDimensions } from "react-icons/rx";
 import Link from 'next/link';
+import FullScreenModal from '@/components/templates/FullScreenModal'; // Import the modal
 
 function Card({ image }) {
 
+    const [isModalOpen, setModalOpen] = useState(false);
+
     const downloadLink = `/downloader/${image.id}?title=${encodeURIComponent(image.title)}&path=${encodeURIComponent(image.path)}`;
+
+    const handleOpenModal = () => {
+        setModalOpen(true); // Open the modal
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false); // Close the modal
+    };
 
     return (
         <div className='card w-full rounded-lg overflow-hidden bg-bgDarkGray'>
             <div className='inside-card w-full px-3 pt-3'>
                 <div className='bg-img relative w-full h-52 flex justify-center items-center p-2 rounded-md'>
-                    <div className='absolute top-2 right-2  bg-darkBlue text-white p-1 rounded-md opacity-60'>
+                    <div className='absolute top-2 right-2 text-white p-1 rounded-md opacity-60 cursor-pointer'
+                        onClick={handleOpenModal}>
                         <MdFullscreen className='text-xl' />
                     </div>
                     <img src={`../../img/${image.path}`} alt={image.fileTitle} className='w-full h-full object-contain' />
@@ -29,14 +41,14 @@ function Card({ image }) {
                             <span className='block ml-1'>{`${image.width} × ${image.height}`}</span>
                         </div>
                     </div>
-                    {/* <button type='button' className='w-full bg-bgDarkBlue rounded mt-3 py-2'>
-                        <Link href={downloadLink}>Download</Link> 
-                    </button> */}
                     <Link href={downloadLink} class="Download-button w-full rounded mt-3 py-2">
                         <span>Download</span>
                     </Link>
                 </div>
             </div>
+
+            {/* Render the FullScreenModal if it is open */}
+            {isModalOpen && <FullScreenModal image={image} onClose={handleCloseModal} />}
         </div>
     );
 }
