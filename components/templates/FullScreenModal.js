99cd077e13672ddffa5f27e0acede3c47dadc7c2
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-22 14:14:26 +0330

    uninstall react-dropzone

--- components/templates/FullScreenModal.js
+++ components/templates/FullScreenModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FullScreenModal = ({ image, onClose }) => {
     return (
-        <div className="fixed inset-0 bg-black flex justify-center items-center z-50">
+        <div className="fixed inset-0 bg-black/[.7] flex justify-center items-center z-50">
             <button onClick={onClose} className="absolute top-4 right-4 text-white text-2xl">✖</button>
             <img src={`../../img/${image.path}`} alt={image.fileTitle} className="max-w-full max-h-full object-contain" />
         </div>
