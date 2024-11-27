a7174d2ca3462997a15f53c251645a7344dbef04
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-28 01:40:30 +0330

    ui footer (WIP)

--- components/templates/Footer.js
+++ components/templates/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
     }, []);
 
     return (
-        <footer className="footer w-full h-48 mt-4">
+        <footer className="footer w-full h-48 mt-8 absolute bottom-0">
             <section className="relative flex flex-col w-full h-full bg-bgDarkGray">
                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
                 text-5xl bg-bgDarkGray text-clDarkBlue flex justify-center items-center">PixelFreeBies</div>
