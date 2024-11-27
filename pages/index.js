b3e32d7aeab12cfcc3d805aac1641c4b83bfa0ee
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-28 02:45:58 +0330

    ui subject-text-header (WIP)

--- pages/index.js
+++ pages/index.js
@@ -271,6 +271,8 @@ function Index() {
                         <svg>
                             <filter id='fire'>
                                 <feTurbulence id='turbulence' baseFrequency='0.1 0.1' numOctaves='2' seed='3'>
+                                    <animate attributeName='baseFrequency' dur='10s' values='0.1 0.1;0.12 0.2'
+                                    repeatCount='indefinite'></animate>
                                 </feTurbulence>
                                 <feDisplacementMap in='SourceGraphic' scale='1'></feDisplacementMap>
                             </filter> 
