96266362c8d43fe2889d6b87a127f1977bc75f4c
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-11-28 14:29:09 +0330

    footer ended

--- components/templates/Footer.js
+++ components/templates/Footer.js
@@ -24,40 +24,13 @@ const Footer = () => {
             <section className="relative flex flex-col w-full h-full bg-bgDarkGray">
                 <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
                 text-5xl bg-bgDarkGray text-clDarkBlue flex justify-center items-center">PixelFreebies</div>
-                <div className="flex flex-nowrap w-full text-clDarkBlue">
-                    {Array.from({ length: iconCount }, (_, index) => (
-                        <TbPng key={index} className="text-5xl -my-3 flex-grow" />
-                    ))}
-                </div>
-                <div className="flex flex-nowrap w-full text-clDarkBlue">
-                    {Array.from({ length: iconCount }, (_, index) => (
-                        <TbPng key={index} className="text-5xl -my-3 flex-grow" />
-                    ))}
-                </div><div className="flex flex-nowrap w-full text-clDarkBlue">
-                    {Array.from({ length: iconCount }, (_, index) => (
-                        <TbPng key={index} className="text-5xl -my-3 flex-grow" />
-                    ))}
-                </div><div className="flex flex-nowrap w-full text-clDarkBlue">
-                    {Array.from({ length: iconCount }, (_, index) => (
-                        <TbPng key={index} className="text-5xl -my-3 flex-grow" /> 
-                    ))}
-                </div><div className="flex flex-nowrap w-full text-clDarkBlue">
-                    {Array.from({ length: iconCount }, (_, index) => (
-                        <TbPng key={index} className="text-5xl -my-3 flex-grow" /> 
-                    ))}
-                </div><div className="flex flex-nowrap w-full text-clDarkBlue">
-                    {Array.from({ length: iconCount }, (_, index) => (
-                        <TbPng key={index} className="text-5xl -my-3 flex-grow" />
-                    ))}
-                </div><div className="flex flex-nowrap w-full text-clDarkBlue">
-                    {Array.from({ length: iconCount }, (_, index) => (
-                        <TbPng key={index} className="text-5xl -my-3 flex-grow" />
-                    ))}
-                </div><div className="flex flex-nowrap w-full text-clDarkBlue">
-                    {Array.from({ length: iconCount }, (_, index) => (
-                        <TbPng key={index} className="text-5xl -my-3 flex-grow" /> 
-                    ))}
-                </div>
+                {Array.from({ length: 8 }, (_, indexRow) => (
+                    <div key={indexRow} className="flex flex-nowrap w-full text-clDarkBlue">
+                        {Array.from({ length: iconCount }, (_, index) => (
+                            <TbPng key={index} className="text-5xl -my-3 flex-grow" />
+                        ))}
+                    </div>
+                ))}
             </section>
         </footer>
     );
