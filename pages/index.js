f270e6d699631bd4ecc2f87fcfde3279b7a2a9e7
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-12-01 21:59:19 +0330

    add favicon in tab

--- pages/index.js
+++ pages/index.js
@@ -258,23 +258,23 @@ function Index() {
                 <title>PixelFreebies</title>
 
                 {/* Favicon for standard browsers */}
-                <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
-                <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-                <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
-                <link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192" />
-                <link rel="icon" type="image/png" href="/favicon-512x512.png" sizes="512x512" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-16x16.png" sizes="16x16" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-32x32.png" sizes="32x32" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-48x48.png" sizes="48x48" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-192x192.png" sizes="192x192" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-512x512.png" sizes="512x512" />
 
                 {/* Apple Touch Icon for iOS devices */}
-                <link rel="apple-touch-icon" href="/favicon-120x120.png" sizes="120x120" />
-                <link rel="apple-touch-icon" href="/favicon-152x152.png" sizes="152x152" />
-                <link rel="apple-touch-icon" href="/favicon-180x180.png" sizes="180x180" />
+                <link rel="apple-touch-icon" href="/img/LOGO-icon-180x180.png" sizes="180x180" />
+                <link rel="apple-touch-icon" href="/img/LOGO-icon-152x152.png" sizes="152x152" />
+                <link rel="apple-touch-icon" href="/img/LOGO-icon-120x120.png" sizes="120x120" />
 
                 {/* Android Chrome Icon */}
-                <link rel="icon" type="image/png" href="/favicon-192x192.png" sizes="192x192" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-192x192.png" sizes="192x192" />
 
-                {/* Microsoft Tiles for Windows */}
-                <meta name="msapplication-TileColor" content="#ffffff" />
-                <meta name="msapplication-TileImage" content="/favicon-270x270.png" />
+                 {/* Microsoft Tiles for Windows */}
+                {/* <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-TileImage" content="/img/LOGO-icon-270x270.png" /> */}
             </Head>
             <div className="app relative">
                 <Header
