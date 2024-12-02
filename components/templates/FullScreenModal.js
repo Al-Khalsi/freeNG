8bed8035da28024206f390b22b4c11c68e7aa07f
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-12-02 14:01:14 +0330

    fix bug full screen mode

--- components/templates/FullScreenModal.js
+++ components/templates/FullScreenModal.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 const FullScreenModal = ({ image, onClose }) => {
     return (
-        <div className="fullScreenModal fixed inset-0 bg-black/[.7] flex justify-center items-center z-50">
-            <button onClick={onClose} className="absolute top-4 right-4 text-white text-2xl">✖</button>
-            <img src={`../../img/${image.path}`} alt={image.fileTitle} className="max-w-full max-h-full object-contain" />
+        <div
+            className="fullScreenModal fixed inset-0 bg-black/[.7] flex justify-center items-center z-50 overflow-hidden"
+            onClick={onClose}
+        >
+            <button onClick={onClose} className="absolute top-8 right-8 text-white text-2xl">✖</button>
+            <div className='relative max-w-full min-h-1 m-auto z-10 overflow-hidden'>
+                    <img
+                        src={`../../img/${image.path}`}
+                        alt={image.fileTitle}
+                        className="max-w-full max-h-full object-contain"
+                        onClick={(e) => e.stopPropagation()}
+                    />
+            </div>
         </div>
     );
 };
