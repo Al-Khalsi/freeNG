1d326f2aa272f1cd1ed9a109c3ee6874b3b99031
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-12-04 03:01:51 +0330

    ui lightMod bg image in card and fulScreen and icon

--- components/templates/FullScreenModal.js
+++ components/templates/FullScreenModal.js
@@ -10,8 +10,8 @@ const FullScreenModal = ({ image, onClose, lightMode }) => {
 
     return (
         <div
-            className={`fullScreenModal fixed inset-0 bg-black/[0.7] 
-            flex justify-center items-center z-50 backdrop-blur-md overflow-hidden ${lightMode ? 'bg-white/[0.5]' : ''}`}
+            className={`fullScreenModal fixed inset-0
+            flex justify-center items-center z-50 backdrop-blur-md overflow-hidden ${lightMode ? 'bg-white/[0.5]' : 'bg-black/[0.7] '}`}
             onClick={onClose}>
             <button onClick={onClose} className="absolute top-8 right-8 text-2xl">✖</button>
             <div className='relative max-w-full h-full m-auto p-8 z-10 overflow-hidden'>
