a3d791b252da99552882f03a34f7ea558d924b1e
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-12-03 22:11:07 +0330

    blur full-screen image modal

--- components/templates/FullScreenModal.js
+++ components/templates/FullScreenModal.js
@@ -3,11 +3,11 @@ import React from 'react';
 const FullScreenModal = ({ image, onClose }) => {
     return (
         <div
-            className="fullScreenModal fixed inset-0 bg-black/[.7] flex justify-center items-center z-50 overflow-hidden"
+            className="fullScreenModal fixed inset-0 bg-black/[.7] flex justify-center items-center z-50 backdrop-blur-md overflow-hidden"
             onClick={onClose}
         >
             <button onClick={onClose} className="absolute top-8 right-8 text-white text-2xl">✖</button>
-            <div className='relative max-w-full h-full m-auto py-8 z-10 overflow-hidden'>
+            <div className='relative max-w-full h-full m-auto p-8 z-10 overflow-hidden'>
                     <img
                         src={`../../img/${image.path}`}
                         alt={image.fileTitle}
