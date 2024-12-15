3c7e16d90fcc746eb61fe700f6ee8d1c89f84a7d
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-12-15 04:18:34 +0330

    add SEO for authentication page

--- pages/authentication/index.js
+++ pages/authentication/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { FaUser, FaEye, FaEyeSlash } from "react-icons/fa";
@@ -116,100 +117,132 @@ function AuthForm() {
     };
 
     return (
-        <div className='Validation w-full h-full flex justify-center items-center min-h-screen bg-bgDarkBlue'>
-            <NotificationContainer />
-            <div className={`wrapper relative w-custom-212 h-custom-136 m-5 bg-bgDarkGray rounded-3xl shadow-xl overflow-hidden ${isActive ? 'active' : ''}`}>
-                <div className='login form-box absolute right-0 w-1/2 h-full flex items-center p-10 bg-gradient-to-t from-bgPurple to-bgLightPurple text-center z-10'>
-                    <form onSubmit={handleLogin} className='w-full'>
-                        <h2 className='text-4xl my-2.5'>Login</h2>
-                        {error && <p className='text-red-500'>{error}</p>}
-                        <div className="inputBox relative my-7 ">
-                            <input type="text"
-                                name="email"
-                                value={credentials.email}
-                                onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium'
-                                placeholder='Email' required />
-                            <FaUser className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
-                        </div>
-                        <div className="inputBox relative my-7">
-                            <input type={isPasswordVisible ? "text" : "password"}
-                                name="password"
-                                value={credentials.password}
-                                onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium placeholder-font-normal'
-                                placeholder='Password' required />
-                            <div
-                                onClick={togglePasswordVisibility}
-                                className='absolute right-5 top-1/2 -translate-y-1/2 
-                                cursor-pointer text-gray-500'>
-                                {isPasswordVisible ? <FaEyeSlash /> : <FaEye /> }
+        <>
+            <Head>
+                <title>Log In / SignUp</title>
+                <link rel="canonical" href={`https://pixelfreebies.com/authentication`} />
+                <meta name="description" content="Log in or create an account on PixelFreebies to access thousands of free PNG images.
+                Join our community and start downloading your favorite images for your projects." />
+                <meta name="keywords" content="PixelFreebies, Log In, SignUp, Free PNG" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="robots" content="index, follow" />
+
+                <meta property="og:title" content="PixelFreebies - Free PNG Images" />
+                <meta property="og:description" content="Explore our extensive collection of free PNG images for your creative projects." />
+                <meta property="og:image" content="../img/PixelFreebies_Banner.jpg" />
+                <meta property="og:url" content="https://pixelfreebies.com" />
+                <meta property="og:type" content="website" />
+
+                <link rel="icon" type="image/png" href="/img/LOGO.png" sizes="16x16" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-32x32.png" sizes="32x32" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-48x48.png" sizes="48x48" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-192x192.png" sizes="192x192" />
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-512x512.png" sizes="512x512" />
+
+                <link rel="apple-touch-icon" href="/img/LOGO-icon-180x180.png" sizes="180x180" />
+                <link rel="apple-touch-icon" href="/img/LOGO-icon-152x152.png" sizes="152x152" />
+                <link rel="apple-touch-icon" href="/img/LOGO-icon-120x120.png" sizes="120x120" />
+
+                <link rel="icon" type="image/png" href="/img/LOGO-icon-192x192.png" sizes="192x192" />
+
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="msapplication-TileImage" content="/img/LOGO-icon-270x270.png" />
+            </Head>
+            <div className='Validation w-full h-full flex justify-center items-center min-h-screen bg-bgDarkBlue'>
+                <NotificationContainer />
+                <div className={`wrapper relative w-custom-212 h-custom-136 m-5 bg-bgDarkGray rounded-3xl shadow-xl overflow-hidden ${isActive ? 'active' : ''}`}>
+                    <div className='login form-box absolute right-0 w-1/2 h-full flex items-center p-10 bg-gradient-to-t from-bgPurple to-bgLightPurple text-center z-10'>
+                        <form onSubmit={handleLogin} className='w-full'>
+                            <h2 className='text-4xl my-2.5'>Login</h2>
+                            {error && <p className='text-red-500'>{error}</p>}
+                            <div className="inputBox relative my-7 ">
+                                <input type="text"
+                                    name="email"
+                                    value={credentials.email}
+                                    onChange={handleChange}
+                                    className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium'
+                                    placeholder='Email' required />
+                                <FaUser className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
                             </div>
-                        </div>
-                        <button type='submit' className='btn w-full h-12 rounded-lg bg-bgDarkBlue text-white shadow-lg border-none text-base font-semibold cursor-pointer'>Login</button>
-
-                    </form>
-                </div>
-
-                <div className='register form-box absolute right-0 w-1/2 h-full flex items-center p-10 bg-gradient-to-t from-bgPurple to-bgLightPurple text-center z-10'>
-                    <form onSubmit={handleRegistration} className='w-full'>
-                        <h2 className='text-4xl my-2.5'>Register</h2>
-                        {error && <p className='text-red-500'>{error}</p>}
-                        <div className="inputBox relative my-7">
-                            <input type="text"
-                                name="username"
-                                value={credentials.username}
-                                onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium'
-                                placeholder='Username' required />
-                            <FaUser className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
-                        </div>
-                        <div className="inputBox relative my-7 ">
-                            <input type="email"
-                                name="email"
-                                value={credentials.email}
-                                onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium'
-                                placeholder='Email' required />
-                            <MdEmail className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
-                        </div>
-                        <div className="inputBox relative my-7">
-                            <input type={isPasswordVisible ? "text" : "password"}
-                                name="password"
-                                value={credentials.password}
-                                onChange={handleChange}
-                                className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium placeholder-font-normal'
-                                placeholder='Password' required />
-                            <div
-                                onClick={togglePasswordVisibility}
-                                className='absolute right-5 top-1/2 -translate-y-1/2 
-                                cursor-pointer text-gray-500'>
-                                {isPasswordVisible ? <FaEyeSlash /> : <FaEye /> }
+                            <div className="inputBox relative my-7">
+                                <input type={isPasswordVisible ? "text" : "password"}
+                                    name="password"
+                                    value={credentials.password}
+                                    onChange={handleChange}
+                                    className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium placeholder-font-normal'
+                                    placeholder='Password' required />
+                                <div
+                                    onClick={togglePasswordVisibility}
+                                    className='absolute right-5 top-1/2 -translate-y-1/2 
+                                    cursor-pointer text-gray-500'>
+                                    {isPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+                                </div>
                             </div>
-                        </div>
-                        <button type='submit'
-                            className='btn w-full h-12 rounded-lg bg-bgDarkBlue text-white shadow-lg border-none text-clBlack text-base font-semibold cursor-pointer'>
-                            Register
-                        </button>
-                    </form>
-                </div>
+                            <button type='submit' className='btn w-full h-12 rounded-lg bg-bgDarkBlue text-white shadow-lg border-none text-base font-semibold cursor-pointer'>Login</button>
 
-                <div className="toggle-form absolute left-0 w-full h-full bg-gradient-to-t from-bgPurple to-bgLightPurple">
-                    <div className='toggle-panel toggle-left absolute left-0 w-1/2 h-full flex flex-col justify-center items-center text-white z-20'>
-                        <h4 className='text-3xl mb-2 hidden md:block'>Hello, Welcome!</h4>
-                        <p className='mb-5 hidden md:block'>Don't have an account?</p>
-                        <button className='btn w-40 h-12 bg-transparent border-2 rounded-lg text-base font-semibold cursor-pointer'
-                            onClick={() => handleClick("register")}>Register</button>
+                        </form>
                     </div>
-                    <div className='toggle-panel toggle-right absolute -right-1/2 w-1/2 h-full flex flex-col justify-center items-center text-white z-20'>
-                        <h4 className='text-3xl mb-2 hidden md:block'>Welcome Back!</h4>
-                        <p className='mb-5 hidden md:block'>Already have an account?</p>
-                        <button className='btn w-40 h-12 bg-transparent border-2 rounded-lg text-base font-semibold cursor-pointer'
-                            onClick={() => handleClick("login")}>Login</button>
+
+                    <div className='register form-box absolute right-0 w-1/2 h-full flex items-center p-10 bg-gradient-to-t from-bgPurple to-bgLightPurple text-center z-10'>
+                        <form onSubmit={handleRegistration} className='w-full'>
+                            <h2 className='text-4xl my-2.5'>Register</h2>
+                            {error && <p className='text-red-500'>{error}</p>}
+                            <div className="inputBox relative my-7">
+                                <input type="text"
+                                    name="username"
+                                    value={credentials.username}
+                                    onChange={handleChange}
+                                    className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium'
+                                    placeholder='Username' required />
+                                <FaUser className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
+                            </div>
+                            <div className="inputBox relative my-7 ">
+                                <input type="email"
+                                    name="email"
+                                    value={credentials.email}
+                                    onChange={handleChange}
+                                    className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium'
+                                    placeholder='Email' required />
+                                <MdEmail className='absolute right-5 top-1/2 -translate-y-1/2 text-gray-500' />
+                            </div>
+                            <div className="inputBox relative my-7">
+                                <input type={isPasswordVisible ? "text" : "password"}
+                                    name="password"
+                                    value={credentials.password}
+                                    onChange={handleChange}
+                                    className='w-full py-3 pr-12 pl-5 bg-bgGray rounded-lg border-none outline-none text-clBlack text-base font-medium placeholder-font-normal'
+                                    placeholder='Password' required />
+                                <div
+                                    onClick={togglePasswordVisibility}
+                                    className='absolute right-5 top-1/2 -translate-y-1/2 
+                                    cursor-pointer text-gray-500'>
+                                    {isPasswordVisible ? <FaEyeSlash /> : <FaEye />}
+                                </div>
+                            </div>
+                            <button type='submit'
+                                className='btn w-full h-12 rounded-lg bg-bgDarkBlue text-white shadow-lg border-none text-clBlack text-base font-semibold cursor-pointer'>
+                                Register
+                            </button>
+                        </form>
                     </div>
-                </div>
+
+                    <div className="toggle-form absolute left-0 w-full h-full bg-gradient-to-t from-bgPurple to-bgLightPurple">
+                        <div className='toggle-panel toggle-left absolute left-0 w-1/2 h-full flex flex-col justify-center items-center text-white z-20'>
+                            <h4 className='text-3xl mb-2 hidden md:block'>Hello, Welcome!</h4>
+                            <p className='mb-5 hidden md:block'>Don't have an account?</p>
+                            <button className='btn w-40 h-12 bg-transparent border-2 rounded-lg text-base font-semibold cursor-pointer'
+                                onClick={() => handleClick("register")}>Register</button>
+                        </div>
+                        <div className='toggle-panel toggle-right absolute -right-1/2 w-1/2 h-full flex flex-col justify-center items-center text-white z-20'>
+                            <h4 className='text-3xl mb-2 hidden md:block'>Welcome Back!</h4>
+                            <p className='mb-5 hidden md:block'>Already have an account?</p>
+                            <button className='btn w-40 h-12 bg-transparent border-2 rounded-lg text-base font-semibold cursor-pointer'
+                                onClick={() => handleClick("login")}>Login</button>
+                        </div>
+                    </div>
+                </div >
             </div >
-        </div >
+        </>
     );
 }
 
