b52b621a31a805a8f411ce1ac96e2c8dbe0fde50
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2024-12-20 15:06:21 +0330

    set PreventDevTools Component for role

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
 import PreventDevTools from '@/components/modules/PreventDevTools';
-import { AuthProvider } from '../context/AuthContext'; 
+import { AuthProvider } from '../context/AuthContext';
+import { useAuth } from '@/context/AuthContext';
 import "../styles/globals.css";
 
 
 function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <PreventDevTools />
+      <AuthConsumer />
       <Component {...pageProps} />
     </AuthProvider>
-  )
+  );
+}
 
+function AuthConsumer() {
+  const { role } = useAuth();
+  return (
+    <>
+      {role !== 'ROLE_MASTER' && <PreventDevTools />}
+    </>
+  );
 }
 
 export default App;
