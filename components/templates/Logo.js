35bfbb8440c1d63b776e7a8d071525ba82db25e0
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-01-03 14:04:21 +0330

    used image component in next for logo

--- components/templates/Logo.js
+++ components/templates/Logo.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import Link from 'next/link';
+import Image from 'next/image';
 
 function Logo() {
     return (
-        <Link href='/' className='w-20 h-20 flex justify-start items-center'>
-            <img src="../../img/LOGO.png" className='w-12 h-12 object-cover 
-            bg-gradient-to-t from-bgPurple to-bgLightPurple rounded-md' alt="Logo" title='Logo' />
+        <Link href='/' className='relative w-14 h-14 flex justify-start items-center'>
+            <Image
+                src="/img/LOGO.png"
+                className='object-cover rounded-md
+                bg-gradient-to-t from-bgPurple to-bgLightPurple'
+                layout="fill"
+                title="Logo"
+                alt="Logo"
+            />
         </Link>
     )
 }
