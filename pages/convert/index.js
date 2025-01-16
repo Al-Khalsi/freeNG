0dcbb08425053a6eea9ab08258f052e295cd1fa8
Al-Khalsi <mohammad.hassan.alkhalsi@gmail.com>
2025-01-16 19:15:13 +0330

    install TypeScript for project

--- pages/convert/index.js
+++ pages/convert/index.js
@@ -4,41 +4,6 @@ import React, { useState } from 'react';
 
 function Convert({ supportedFormats }) {
 
-  const extensions = {
-    image: [
-      "jpg",
-      "jpeg",
-      "png",
-      "gif",
-      "bmp",
-      "webp",
-      "ico",
-      "tif",
-      "tiff",
-      "svg",
-      "raw",
-      "tga",
-    ],
-    video: [
-      "mp4",
-      "m4v",
-      "mp4v",
-      "3gp",
-      "3g2",
-      "avi",
-      "mov",
-      "wmv",
-      "mkv",
-      "flv",
-      "ogv",
-      "webm",
-      "h264",
-      "264",
-      "hevc",
-      "265",
-    ],
-    audio: ["mp3", "wav", "ogg", "aac", "wma", "flac", "m4a"],
-  };
 
   return (
     <MainLayout
